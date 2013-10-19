21e789140af6dac5f0e6aa8864843fa294046ea4
Luzi82 <luzi82@gmail.com>
2013-10-19 10:55:18 +0800

    add unsolved requirejs test

---
+++ res/rjs_b.js
@@ -0,0 +1,5 @@
+define(['rjs_a'],function(rjs_a){
+	return {
+		hello_b: function(){return rjs_a.hello_a();}
+	}
+});
---
+++ src/com/luzi82/rhinotest/RequireJsTest.java
@@ -0,0 +1,38 @@
+package com.luzi82.rhinotest;
+
+import java.io.FileReader;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mozilla.javascript.Callable;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+public class RequireJsTest {
+
+	/**
+	 * still fail...
+	 * leave this alone for future solve
+	 * @throws Exception
+	 */
+	@Test
+	public void rjstest0() throws Exception {
+		Context cx = Context.enter();
+		cx.setOptimizationLevel(-1); // avoid the 64k limit... not too good...
+
+		Scriptable scope = cx.initStandardObjects();
+		
+		cx.evaluateReader(scope,
+				new FileReader("res/r.js"), "res/r.js", 0, null);
+
+		Object result = cx.evaluateReader(scope,
+				new FileReader("res/rjs_b.js"), "res/rjs_b.js", 0, null);
+
+		Scriptable sa = (Scriptable) result;
+		Callable hello_b = (Callable) sa.get("hello_b", sa);
+		Assert.assertEquals("HelloWorld", hello_b.call(cx, scope, null, null));
+
+		Context.exit();
+	}
+
+}
